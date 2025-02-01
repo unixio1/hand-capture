b42f3f4b6698cb682f3d34948273a4886b9c3915
unixio1 <unaxag01@gmail.com>
2025-02-01 11:42:51 +0100

    Multiple convolution layer implementation

--- src/model/conv.py
+++ src/model/conv.py
@@ -1,13 +1,15 @@
+from typing import List
 from torch import nn, Tensor
 
 class ConvLayer(nn.Module):
 
-        def __init__(self, in_channels: int, out_channels: int, kernel_size: int, stride: int, padding: int):
+        def __init__(self, in_channels: int, out_channels: int, kernel_size: int, stride: int = 1, padding: int = 0):
                 super(ConvLayer, self).__init__()
                 self.conv2d = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=True)
                 self.maxpool = nn.MaxPool2d(kernel_size, stride, padding)
                 self.batchnorm = nn.BatchNorm2d(out_channels)
                 self.activation = nn.LeakyReLU(0.1)
+        
         def forward(self, x: Tensor, pooling: bool = True, normalize: bool = True):
                 x = self.conv2d(x)
                 if pooling:
@@ -15,3 +17,16 @@ class ConvLayer(nn.Module):
                 if normalize:
                         x = self.batchnorm(x)
                 return self.activation(x)
+
+class MultiConvLayer(nn.Module):
+
+        def __init__(self, n_convolutions: int, in_channels: int, out_channels: List[int], kernel_sizes: List[int]):
+                self.convolutions = []
+                for i in range(n_convolutions):
+                        self.convolutions.append(ConvLayer(in_channels, out_channels[i], kernel_sizes[i]))
+                        in_channels = out_channels[i]
+
+        def forward(self, x: Tensor) -> Tensor:
+                for i in range(len(self.convolutions) - 1):
+                        x = self.convolutions[i].forward(x, pooling = False)
+                return self.convolutions[-1].forward(x, pooling = True)
--- tests/model/test_conv.py
+++ tests/model/test_conv.py
@@ -1,4 +1,4 @@
-from model.conv import ConvLayer
+from model.conv import ConvLayer, MultiConvLayer
 from torch import Tensor
 from torch import Size
 
@@ -13,19 +13,35 @@ conv_layer = ConvLayer(in_channels, out_channels, kernel_size, stride, padding)
 def test_conv_layer(resized_image: Tensor):
     image_shape = list(resized_image.shape)
     new_image_size = ((resized_image.shape[3] - kernel_size + 2 * padding) // stride) + 1
-    output_shape = Size([*image_shape[:2], new_image_size, new_image_size])
+    output_shape = Size([image_shape[0], out_channels, new_image_size, new_image_size])
     conv_output = conv_layer.forward(resized_image, pooling=False)
     assert conv_output.shape == output_shape
 
 def test_conv_pooled_layer(resized_image: Tensor):
     image_shape = list(resized_image.shape)
     new_image_size = ((resized_image.shape[3] - kernel_size + 2 * padding) // stride) + 1
-    conv_output_shape = [*image_shape[:2], new_image_size, new_image_size]
+    conv_output_shape = [image_shape[0], out_channels, new_image_size, new_image_size]
     pool_output_size = ((conv_output_shape[3] - kernel_size + 2 * padding) // stride) + 1
     pool_output_shape = Size([*conv_output_shape[:2], pool_output_size, pool_output_size])
     output = conv_layer.forward(resized_image, pooling=True)
     assert output.shape == pool_output_shape 
     
 
-
-
+def test_multi_conv_layer(resized_image: Tensor):
+    image_shape = list(resized_image.shape)
+    n_convolutions = 3
+    kernel_sizes = [1, 3, 3]
+    out_channels = [128, 256, 3]
+    multi_conv_layer = MultiConvLayer(
+        n_convolutions=n_convolutions,
+        in_channels=1,
+        out_channels=out_channels,
+        kernel_sizes=kernel_sizes
+    )
+    new_size = image_shape[3]
+    for i in range(n_convolutions):
+        new_size = (new_size - kernel_sizes[i]) + 1
+    new_size = (new_size - kernel_sizes[-1]) + 1 #This is regarding the last convolution involves a pooling, so we must calculate the ouptut of this operation
+    pool_output_shape = Size([image_shape[0], out_channels[-1], new_size, new_size])
+    output = multi_conv_layer.forward(resized_image)
+    assert output.shape == pool_output_shape
