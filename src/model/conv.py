28129f1650e4e0f263e7931cdf932e2c0c811bef
unixio1 <unaxag01@gmail.com>
2025-02-01 12:23:31 +0100

    Convolutional layer input refactoring

--- src/model/conv.py
+++ src/model/conv.py
@@ -1,15 +1,46 @@
-from typing import List
+from typing import List, Optional
+from dataclasses import dataclass
 from torch import nn, Tensor
+
+@dataclass
+class ConvLayerInputModel:
+
+        in_channels: int
+        out_channels: int
+        kernel_size: int
+        stride: int = 1
+        padding: int = 0
+
+def create_multi_conv_layer_input(
+        in_channels: int,
+        out_channels: List[int],
+        kernel_sizes: List[int],
+        strides: Optional[List[int]] = None,
+        paddings: Optional[List[int]] = None
+) -> List[ConvLayerInputModel]:
+        input = []
+        for i in range(len(out_channels)):
+                input.append(
+                        ConvLayerInputModel(
+                                in_channels=in_channels,
+                                out_channels=out_channels[i],
+                                kernel_size=kernel_sizes[i],
+                                stride=strides[i] if strides else 1,
+                                padding=paddings[i] if paddings else 0
+                        )
+                )
+                in_channels = out_channels[i]
+        return input
 
 class ConvLayer(nn.Module):
 
-        def __init__(self, in_channels: int, out_channels: int, kernel_size: int, stride: int = 1, padding: int = 0):
+        def __init__(self, input: ConvLayerInputModel):
                 super(ConvLayer, self).__init__()
-                self.conv2d = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=True)
-                self.maxpool = nn.MaxPool2d(kernel_size, stride, padding)
-                self.batchnorm = nn.BatchNorm2d(out_channels)
+                self.conv2d = nn.Conv2d(input.in_channels, input.out_channels, input.kernel_size, input.stride, input.padding, bias=True)
+                self.maxpool = nn.MaxPool2d(input.kernel_size, input.stride, input.padding)
+                self.batchnorm = nn.BatchNorm2d(input.out_channels)
                 self.activation = nn.LeakyReLU(0.1)
-        
+
         def forward(self, x: Tensor, pooling: bool = True, normalize: bool = True):
                 x = self.conv2d(x)
                 if pooling:
@@ -20,11 +51,10 @@ class ConvLayer(nn.Module):
 
 class MultiConvLayer(nn.Module):
 
-        def __init__(self, n_convolutions: int, in_channels: int, out_channels: List[int], kernel_sizes: List[int]):
+        def __init__(self, input: List[ConvLayerInputModel]):
                 self.convolutions = []
-                for i in range(n_convolutions):
-                        self.convolutions.append(ConvLayer(in_channels, out_channels[i], kernel_sizes[i]))
-                        in_channels = out_channels[i]
+                for model in input:
+                        self.convolutions.append(ConvLayer(model))
 
         def forward(self, x: Tensor) -> Tensor:
                 for i in range(len(self.convolutions) - 1):
--- tests/model/test_conv.py
+++ tests/model/test_conv.py
@@ -1,4 +1,5 @@
-from model.conv import ConvLayer, MultiConvLayer
+from typing import List
+from model.conv import ConvLayerInputModel, ConvLayer, MultiConvLayer, create_multi_conv_layer_input
 from torch import Tensor
 from torch import Size
 
@@ -8,40 +9,43 @@ padding = 0 # In case we want to mantain the size of the input, we add padding t
 stride = 1 #How the kernel moves, the step size (1 means it moves pixel by pixel applying convolutions)
 in_channels = 1 #1 for grayscale, 3 for RGB
 out_channels = 1
-conv_layer = ConvLayer(in_channels, out_channels, kernel_size, stride, padding)
+conv_layer_input = ConvLayerInputModel(
+    in_channels=in_channels,
+    out_channels=out_channels,
+    kernel_size=kernel_size,
+    stride=stride,
+    padding=padding
+)
+conv_layer = ConvLayer(conv_layer_input)
 
 def test_conv_layer(resized_image: Tensor):
-    image_shape = list(resized_image.shape)
-    new_image_size = ((resized_image.shape[3] - kernel_size + 2 * padding) // stride) + 1
-    output_shape = Size([image_shape[0], out_channels, new_image_size, new_image_size])
+    expected_shape = _calculate_shape_after_convolution(resized_image, [out_channels], [kernel_size], pooling=False)
     conv_output = conv_layer.forward(resized_image, pooling=False)
-    assert conv_output.shape == output_shape
+    assert conv_output.shape == expected_shape
 
 def test_conv_pooled_layer(resized_image: Tensor):
-    image_shape = list(resized_image.shape)
-    new_image_size = ((resized_image.shape[3] - kernel_size + 2 * padding) // stride) + 1
-    conv_output_shape = [image_shape[0], out_channels, new_image_size, new_image_size]
-    pool_output_size = ((conv_output_shape[3] - kernel_size + 2 * padding) // stride) + 1
-    pool_output_shape = Size([*conv_output_shape[:2], pool_output_size, pool_output_size])
+    expected_shape = _calculate_shape_after_convolution(resized_image, [out_channels], [kernel_size])
     output = conv_layer.forward(resized_image, pooling=True)
-    assert output.shape == pool_output_shape 
-    
+    assert output.shape == expected_shape 
+
 
 def test_multi_conv_layer(resized_image: Tensor):
-    image_shape = list(resized_image.shape)
-    n_convolutions = 3
     kernel_sizes = [1, 3, 3]
     out_channels = [128, 256, 3]
     multi_conv_layer = MultiConvLayer(
-        n_convolutions=n_convolutions,
-        in_channels=1,
-        out_channels=out_channels,
-        kernel_sizes=kernel_sizes
+        create_multi_conv_layer_input(in_channels=in_channels, out_channels=out_channels, kernel_sizes=kernel_sizes)
     )
-    new_size = image_shape[3]
-    for i in range(n_convolutions):
-        new_size = (new_size - kernel_sizes[i]) + 1
-    new_size = (new_size - kernel_sizes[-1]) + 1 #This is regarding the last convolution involves a pooling, so we must calculate the ouptut of this operation
-    pool_output_shape = Size([image_shape[0], out_channels[-1], new_size, new_size])
+    expected_shape = _calculate_shape_after_convolution(resized_image, out_channels, kernel_sizes)
     output = multi_conv_layer.forward(resized_image)
-    assert output.shape == pool_output_shape
+    assert output.shape == expected_shape
+
+def _calculate_shape_after_convolution(image: Tensor, out_channels: List[int], kernel_sizes: List[int], pooling: bool = True) -> Size:
+    assert len(out_channels) == len(kernel_sizes)
+    image_shape = list(image.shape)
+    new_size = image_shape[3]
+    for i in range(len(out_channels)):
+        new_size = (new_size - kernel_sizes[i]) + 1
+    if pooling:
+        new_size = (new_size - kernel_sizes[-1]) + 1 #This is regarding the last convolution involves a pooling, so we must calculate the ouptut of this operation
+    return Size([image_shape[0], out_channels[-1], new_size, new_size])
+
